a5558eddb0763e39a2ac3b39bdb54994a8259759
Przydan <patryk@przydanek.com>
2019-11-04 11:24:10 +0100

    TODO notes

--- src/main/java/pl/przydan/App.java
+++ src/main/java/pl/przydan/App.java
@@ -9,23 +9,26 @@ import static java.lang.Character.toUpperCase;
  * Morse'a translator
  * by Przydan
  */
+// TODO: reverse translation, make factory, add secondLanguage
+
 public class App {
     public static void main(String[] args) {
+
         Dictionary dictionary = new Morse_aDictionary();
         StringBuilder stringBuilder = new StringBuilder();
-
 
         String word = "test sos";
         String translatedWord = "";
 
         for (int i = 0; i < word.length(); i++) {
-            Character character = word.charAt(i);
+            char character = word.charAt(i);
             translatedWord = stringBuilder.append(dictionary.translate(toUpperCase(character))).append(" ").toString();
         }
 
-        System.out.println("Podane wyrażenie do przetłumaczenia to: " + word);
+        System.out.println("Wyrażenie do przetłumaczenia: " + word);
 
         System.out.println("Wyrażenie zapisane alfabetem morsa: " + translatedWord);
+
 
     }
 }
